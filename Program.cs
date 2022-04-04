8ce88b9b819f8ce4781c482b7666aa0d3fa33fae
[chetan deore] <[deorec346@gmail.com]>
2022-04-04 23:15:12 +0530

    Refactor

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorProb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+            EmployeeWage employee = new EmployeeWage();
+            employee.Condition();
+            employee.Display();
+            Console.ReadLine();
+        }
+    }
+}
